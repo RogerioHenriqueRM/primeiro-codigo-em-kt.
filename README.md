ba2243b10aff2247e79faf3f33526e46e1b6f62d
rogerio henrique rocha de matos <rog.henr16@gmail.com>
2025-05-14 21:24:06 -0400

    Salvar alterações antes do pull

--- README.md
+++
@@ -1,2 +0,0 @@
-# primeiro-codigo-em-kt.
-esa é meinha primeira publicação em kotlin
---
+++ src/main/kotlin/HelloWorld.kt
@@ -0,0 +1,11 @@
+/*fun main(){
+    println("hello, World")
+    hello()
+    println(hello)
+} */
+
+
+fun hello(): String {
+    return "Hello, World!"
+    
+}
